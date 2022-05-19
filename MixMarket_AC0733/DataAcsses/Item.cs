1d420d2bed277c100df1becaa05e1e5de4765263
محمد ابو ارميله <AC0733@iu.edu.jo>
2022-05-19 12:53:07 +0300

    assinmet

---
+++ MixMarket_AC0733/DataAcsses/Item.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MixMarket_AC0733.DataAcsses
+{
+    public class Item
+    {
+        public int ItemId { get; set; }
+        public string ItemName { get; set; }
+    }
+}
---
+++ MixMarket_AC0733/DataAcsses/ItemDAL.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MixMarket_AC0733.DataAcsses
+{
+    public static class ItemDAL
+    {
+        public static List<Item> GetAllItems()
+        {
+            // List<Item> items = new List<Item>();
+            //return items;
+            return null;
+        }
+        public static Item GetItem(int id)
+        {
+            return null;
+        }
+        public static void creatItem(Item item)
+        {
+
+        }
+        public static void UpdateItem(Item item)
+        {
+
+        }
+        public static void DleteItem(Item item)
+        {
+
+        }
+    }
+}
